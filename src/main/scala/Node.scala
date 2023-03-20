0ff92c3bcc83736a26f22c6a5bfbf514a890a99b
tamdnguyen <tamdnguyen2111@gmail.com>
2023-03-20 02:03:12 +0200

    initialize Node and LinkedList class
    draft version - needs a lot of modification and improvement

---
+++ src/main/scala/Node.scala
@@ -0,0 +1,22 @@
+import engine.grid
+import engine.grid.GameBoard
+
+import scala.collection.immutable.TreeSeqMap.Ordering.Iterator.Empty.next
+
+
+class Node(val id: Int, var prev: Option[Node] = None, var next: Option[Node] = None)
+  val gameBoard: GameBoard = new GameBoardObject
+  val waitingBoard: WaitingBoard = new WaitingBoardObject
+
+  def hasNext: Boolean = next.isDefined
+
+  def hasPrev: Boolean = prev.isDefined
+
+  def getNext: Option[Node] = next
+
+  def getPrev: Option[Node] = prev
+
+  def setNext(node: Option[Node]): Unit = next = node
+
+  def setPrev(node: Option[Node]): Unit = prev = node
+
