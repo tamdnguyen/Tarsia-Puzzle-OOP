9509bf733bd076beae46aa68e1e4e6faf9efa865
tamdnguyen <tamdnguyen2111@gmail.com>
2023-04-10 20:23:53 +0300

    update some constants

--- src/main/scala/engine/grid/package.scala
+++ src/main/scala/engine/grid/package.scala
@@ -1,9 +1,35 @@
 package engine.grid
 
-package object grid {
-  var edgeLength: Double = 1.0 // edgeLength of triangles in the hexagonal grid
-  val coeff: Double = math.sqrt(3) // coefficient used for triangular calculation
+package object grid:
+  
+  /**
+    * Edge length of triangles in the hexagonal grid.
+    */
+  var edgeLength: Double = 1.0 
 
+  /**
+    * Coefficient used for triangular calculation.
+    */
+  val coeff: Double = math.sqrt(3) 
+
+  /**
+    * Declaration of matching values for the edges.
+    */
+  val matchingEdges: Vector[(Int, Int)] = 
+    Vector((1,11),
+           (2,22),
+           (3,33),
+           (4,44))
+  
+  /**
+    * Return a list of all possible values for triangle edges.
+    */
+  val edgeValues: Vector[Int] = 
+    this.matchingEdges.flatMap((x,y) => Seq(x,y))
+
+  /**
+    * HashMap convert `GridPos` coordinates to `TriGridPos` coordinates.
+    */
   val gridToTriGrid: Map[(Int, Int), (Int, Int, Int)] =
     Map((0,0) -> (-1,2,1),
         (0,1) -> (-1,2,0),
@@ -30,7 +56,10 @@ package object grid {
         (3,3) -> (2,-1,1),
         (3,4) -> (2,-1,0))
 
+  /**
+    * HashMap convert `TriGridPos` coordinates to `GridPos` coordinates.
+    */
   val triGridToGrid: Map[(Int, Int, Int), (Int, Int)] =
     gridToTriGrid.map(_.swap)
 
-}
+end grid
