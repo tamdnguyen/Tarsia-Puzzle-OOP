c5d6eac8cacde6f94d58e1f890c00c5537f08e54
tamdnguyen <tamdnguyen2111@gmail.com>
2023-04-15 20:14:02 +0300

    tooltip

--- src/main/scala/gui/BoardPanel.scala
+++ src/main/scala/gui/BoardPanel.scala
@@ -21,13 +21,14 @@ class BoardPanel(val board: Board, val centerX: Int, val centerY: Int) extends F
     */
   val cornersShifted = holderCorners.map(_.map(_.shift(centerX, centerY)))
 
+
   /**
     * Add the tooltip showing the mouse position on the panel
     */
   listenTo(mouse.moves)
   reactions += {
     case e: MouseMoved =>
-      peer.setToolTipText(s"Mouse position: (${e.point.x}, ${e.point.y})")
+      peer.setToolTipText(s"${board}: (${e.point.x}, ${e.point.y})")
       repaint()
   }
   ToolTipManager.sharedInstance().setInitialDelay(0)
@@ -41,6 +42,7 @@ class BoardPanel(val board: Board, val centerX: Int, val centerY: Int) extends F
     g.setColor(Color.RED)
     g.fillOval(centerX - 5, centerY - 5, 10, 10)
     g.drawOval(centerX - 50, centerY - 50, 100, 100)
+
     // draw the TriHolders with dashed-lines
     g.setStroke(new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, Array(5.0f), 0.0f))
     g.setColor(Color.BLACK)
@@ -50,6 +52,18 @@ class BoardPanel(val board: Board, val centerX: Int, val centerY: Int) extends F
         g.draw(new Line2D.Double(p2.x, p2.y, p3.x, p3.y))
         g.draw(new Line2D.Double(p3.x, p3.y, p1.x, p1.y))
       case _ => // handle case where Vector[Point] has fewer or more than three points
+    }
+
+    // draw the edges
+    g.setStroke(new BasicStroke(2.0f))
+    board.tileList.foreach { triTile =>
+      triTile.edges.foreach { edge =>
+        val p1 = edge.p1.shift(centerX, centerY)
+        val p2 = edge.p2.shift(centerX, centerY)
+        val color = ColorMapper(edge.value)
+        g.setColor(color)
+        g.draw(new Line2D.Double(p1.x, p1.y, p2.x, p2.y))
+      }
     }
   }
 
