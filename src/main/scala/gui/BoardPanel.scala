1c423b4e1b4a8d45f6aa42ce56463735027dfd3f
tamdnguyen <tamdnguyen2111@gmail.com>
2023-04-16 22:48:45 +0300

    tooltip refer to the correct triangle tiles

--- src/main/scala/gui/BoardPanel.scala
+++ src/main/scala/gui/BoardPanel.scala
@@ -2,9 +2,9 @@ package gui
 
 import engine._
 import scala.swing._
-import scala.swing.event.MouseMoved
+import scala.swing.event.{MouseMoved, MouseClicked, MousePressed, MouseDragged}
 import javax.swing.ToolTipManager
-import java.awt.{Color, BasicStroke}
+import java.awt.{Color, Polygon, BasicStroke}
 import java.awt.geom.Line2D
 
 
@@ -17,31 +17,36 @@ class BoardPanel(val board: Board, val centerX: Int, val centerY: Int) extends F
 
 
   /**
-    * Shift the corners coordinates according to the center point of this panel
+    * Shift the corners coordinates according to the center point of this panel.
+    * 
+    * First, we perform reflection because engine.grid increases y northward while
+    * scala.swing increases y southward.
     */
-  val cornersShifted = holderCorners.map(_.map(_.shift(centerX, centerY)))
+  val cornersShifted = holderCorners.map(_.map(_.reflectY().shift(centerX, centerY)))
 
 
   /**
     * Add the tooltip showing the mouse position on the panel
     */
-  listenTo(mouse.moves)
+  listenTo(mouse.moves, mouse.clicks)
   reactions += {
     case e: MouseMoved =>
-      peer.setToolTipText(s"${board}: (${e.point.x}, ${e.point.y})")
+      val x = e.point.x
+      val y = e.point.y
+      val (tile, gridPos) = board.pickTile(engine.grid.Point(x - centerX, -y + centerY))
+      // peer.setToolTipText(s"${board}: (${e.point.x}, ${e.point.y})")
+      peer.setToolTipText(s"${board}: (${tile}, ${gridPos})")
       repaint()
   }
   ToolTipManager.sharedInstance().setInitialDelay(0)
   ToolTipManager.sharedInstance().setDismissDelay(1000)
+
 
   /**
     * Draw the components of the panel
     */
   override def paintComponent(g: Graphics2D): Unit = {
     super.paintComponent(g)
-    g.setColor(Color.RED)
-    g.fillOval(centerX - 5, centerY - 5, 10, 10)
-    g.drawOval(centerX - 50, centerY - 50, 100, 100)
 
     // draw the TriHolders with dashed-lines
     g.setStroke(new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, Array(5.0f), 0.0f))
@@ -58,8 +63,8 @@ class BoardPanel(val board: Board, val centerX: Int, val centerY: Int) extends F
     g.setStroke(new BasicStroke(2.0f))
     board.tileList.foreach { triTile =>
       triTile.edges.foreach { edge =>
-        val p1 = edge.p1.shift(centerX, centerY)
-        val p2 = edge.p2.shift(centerX, centerY)
+        val p1 = edge.p1.reflectY().shift(centerX, centerY)
+        val p2 = edge.p2.reflectY().shift(centerX, centerY)
         val color = ColorMapper(edge.value)
         g.setColor(color)
         g.draw(new Line2D.Double(p1.x, p1.y, p2.x, p2.y))
