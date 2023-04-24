afd37aeb518e59e080a985defd2a584870e0f029
tamdnguyen <tamdnguyen2111@gmail.com>
2023-04-24 21:49:33 +0300

    initialize the loadGame() method, still have bug

--- src/main/scala/engine/Game.scala
+++ src/main/scala/engine/Game.scala
@@ -5,6 +5,9 @@ import java.io.{File, PrintWriter}
 import org.json4s._
 import org.json4s.jackson.Serialization
 import org.json4s.jackson.Serialization.{write, writePretty}
+import io.circe._
+import io.circe.parser._
+import io.circe.generic.semiauto._
 
 
 class Game:
@@ -38,21 +41,21 @@ class Game:
   def saveGame(filename: String): Unit = 
     implicit val formats: DefaultFormats.type = DefaultFormats
     val gameBoardTiles = gameBoard.tileList.map(t => Map(
-      "a" -> t.a.toString,
-      "b" -> t.b.toString,
-      "c" -> t.c.toString,
-      "edges" -> t.values.map(_.toString)
+      "a" -> t.a,
+      "b" -> t.b,
+      "c" -> t.c,
+      "edges" -> t.values
     ))
     val waitingBoardTiles = waitingBoard.tileList.map(t => Map(
-      "a" -> t.a.toString,
-      "b" -> t.b.toString,
-      "c" -> t.c.toString,
-      "edges" -> t.values.map(_.toString)
+      "a" -> t.a,
+      "b" -> t.b,
+      "c" -> t.c,
+      "edges" -> t.values
     ))
     val gameData = Map(
-      "MoveCount" -> this.moveCount.toString(),
-      "GameBoard" -> gameBoardTiles,
-      "WaitingBoard" -> waitingBoardTiles
+      "moveCount" -> this.moveCount,
+      "gameBoard" -> gameBoardTiles,
+      "waitingBoard" -> waitingBoardTiles
     )
     val jsonString = writePretty(gameData)
     val file = new File(filename)
@@ -62,7 +65,63 @@ class Game:
 
 
 
-  def loadGame(): Unit = ???
+  def loadGame(filename: String): (Boolean, String) = 
+    val gameBoard = new GameBoard()
+    val waitingBoard = new WaitingBoard()
+    var moveCount: Int = -1
+
+    // Load JSON data from file
+    val file = new File(filename)
+    if (!file.exists() || !file.isFile || !filename.endsWith(".json")) then
+      return (false, s"Invalid file: $filename")
+
+    val json = scala.io.Source.fromFile(file).mkString
+
+    implicit val tileDecoder: Decoder[TriTileJSON] = deriveDecoder[TriTileJSON]
+    implicit val gameDecoder: Decoder[GameDataJSON] = deriveDecoder[GameDataJSON]
+
+    val decoded = decode[GameDataJSON](json)
+    println(decoded)
+
+    decoded match
+      case Right(gameData) =>
+        try // try to assign the game move count
+          moveCount = gameData.moveCount
+        catch
+          case e: Exception => return (false, s"Invalid data for move count: ${e.getMessage()}")
+        try // try to update the tiles of the game board
+          gameData.gameBoard.foreach( tileData =>
+            val a: Int = tileData.a 
+            val b: Int = tileData.b
+            val c: Int = tileData.c 
+            val edges: List[Int] = tileData.edges
+            val tile = new TriTile(a,b,c)
+            tile.updateEdgeValues(edges(0), edges(1), edges(2))
+            gameBoard.addTile(tile, tile.pos)
+          )
+        catch
+          case e: Exception => return (false, s"Invalid data for game board: ${e.getMessage()}")
+        try // try to update the tiles of the waiting board
+          gameData.waitingBoard.foreach( tileData =>
+            val a: Int = tileData.a 
+            val b: Int = tileData.b
+            val c: Int = tileData.c 
+            val edges: List[Int] = tileData.edges
+            val tile = new TriTile(a,b,c)
+            tile.updateEdgeValues(edges(0), edges(1), edges(2))
+            waitingBoard.addTile(tile, tile.pos)
+          )
+        catch
+          case e: Exception => return (false, s"Invalid data for waiting board: ${e.getMessage()}")
+      case _ => return (false, "Invalid game data")
+
+    // update the game to the loaded game
+    this.moveCount = moveCount
+    this.gameBoard = gameBoard
+    this.waitingBoard = waitingBoard
+
+    (true, "Game loaded successfully!")
+
   
 
   /**
@@ -71,6 +130,13 @@ class Game:
   def advance() = 
     this.moveCount += 1
 
+
+  /**
+    * Return the status of the game.
+    * 
+    * Move count if the game is not completed yet.
+    * Otherwise, a congratulation text.
+    */
   def status(): String =
     if this.gameBoard.isCompleted then
       "Congratulations! You have won the game!"
